0fe30fa10b82b0c23cafce7b2adaa3bdffcfa59f
PhuongN08 <nguyenluongthephuong14@gmail.com>
2024-10-20 10:49:19 +0700

    Fragment update

--- app/src/main/java/com/example/doctruyenapplication/AccountFragment.java
+++ app/src/main/java/com/example/doctruyenapplication/AccountFragment.java
@@ -1,4 +1,14 @@
 package com.example.doctruyenapplication;
 
-public class AccountFragment {
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+public class AccountFragment extends  Fragment{
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Nạp bố cục cho fragment này
+        return inflater.inflate(R.layout.fragment_account, container, false);
+    }
 }
--- app/src/main/java/com/example/doctruyenapplication/BookselfFragment.java
+++ app/src/main/java/com/example/doctruyenapplication/BookselfFragment.java
@@ -1,4 +1,15 @@
 package com.example.doctruyenapplication;
 
-public class BookselfFragment {
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+public class BookshelfFragment extends  Fragment{
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Nạp bố cục cho fragment này
+        return inflater.inflate(R.layout.fragment_bookself, container, false);
+    }
 }
