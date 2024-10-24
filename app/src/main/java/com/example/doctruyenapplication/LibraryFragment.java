1173f2bc741c2a93bb8e38da0a6df8ecc88936e4
PhuongN08 <nguyenluongthephuong14@gmail.com>
2024-10-24 09:15:47 +0700

    adapter_almost

--- app/src/main/java/com/example/doctruyenapplication/LibraryFragment.java
+++ app/src/main/java/com/example/doctruyenapplication/LibraryFragment.java
@@ -5,14 +5,19 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.GridView;
+import android.widget.ImageButton;
+import android.widget.TextView;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
 import com.example.doctruyenapplication.adapter.BookAdapter;
 import com.example.doctruyenapplication.object.Book;
 import java.util.ArrayList;
 
 public class LibraryFragment extends Fragment {
 
-    private GridView newStoriesGridView;
+    private GridView newStoriesGridView, bestStoriesGridView, anStoriesGridView;
     private ArrayList<Book> bookList;
     private BookAdapter bookAdapter;
 
@@ -23,17 +28,46 @@ public class LibraryFragment extends Fragment {
 
         // Initialize GridView
         newStoriesGridView = view.findViewById(R.id.new_stories_grid);
+        bestStoriesGridView = view.findViewById(R.id.best_stories_grid);
+        anStoriesGridView = view.findViewById(R.id.an_stories_grid);
 
         // Initialize book list and add some sample data (you can replace this with actual data)
         bookList = new ArrayList<>();
-        bookList.add(new Book("https://ddntcthcd.com/nettruyen/thumb/cao-vo-ha-canh-den-mot-van-nam-sau.jpg", "Boku no Pico", "Chapter 1"));
-        bookList.add(new Book("https://ddntcthcd.com/nettruyen/thumb/vo-dich-don-ngo.jpg", "Two black man kissing", "Chapter 2"));
-        bookList.add(new Book("https://kcgsbok.com/nettruyen/thumb/ta-co-mot-son-trai.jpg", "Why are you gay", "Chapter 3"));
+        bookList.add(new Book("https://cdn.myanimelist.net/images/anime/12/39497.jpg", "Boku no Pico", "Chapter 1"));
+        bookList.add(new Book("https://a.pinatafarm.com/500x377/d972ce254e/2-gay-black-mens-kissing.jpg", "Two black man kissing", "Chapter 2"));
+        bookList.add(new Book("https://ih1.redbubble.net/image.4595760308.2867/flat,750x,075,f-pad,750x1000,f8f8f8.jpg", "Why are you gay", "Chapter 3"));
 
         // Create an adapter and set it to the GridView
         bookAdapter = new BookAdapter(getActivity(), R.layout.item_book, bookList);
         newStoriesGridView.setAdapter(bookAdapter);
+        bestStoriesGridView.setAdapter(bookAdapter);
+        anStoriesGridView.setAdapter(bookAdapter);
+
+        // Set up click listeners
+        setupClickListeners(view);
 
         return view;
+    }
+
+    private void setupClickListeners(View view) {
+        ImageButton searchButton = view.findViewById(R.id.search_button);
+        searchButton.setOnClickListener(v -> navigateToFragment(new SearchFragment()));
+
+        TextView viewMoreNewStories = view.findViewById(R.id.view_more_button);
+        viewMoreNewStories.setOnClickListener(v -> navigateToFragment(new MoreStoriesFragment()));
+
+        TextView viewMoreBestStories = view.findViewById(R.id.view_more_button1);
+        viewMoreBestStories.setOnClickListener(v -> navigateToFragment(new MoreStoriesFragment()));
+
+        TextView viewMoreAwardStories = view.findViewById(R.id.view_more_button2);
+        viewMoreAwardStories.setOnClickListener(v -> navigateToFragment(new MoreStoriesFragment()));
+    }
+
+    private void navigateToFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.container, fragment); // Replace with your container ID
+        fragmentTransaction.addToBackStack(null); // Optional: add to back stack
+        fragmentTransaction.commit();
     }
 }
