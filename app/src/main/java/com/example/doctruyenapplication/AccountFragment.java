a809c7da442b27871db9389083f1645f7ac7256c
Mon1712 <133070020+Mon1712@users.noreply.github.com>
2024-11-01 08:47:14 +0700

    login logout cnt

--- app/src/main/java/com/example/doctruyenapplication/AccountFragment.java
+++ app/src/main/java/com/example/doctruyenapplication/AccountFragment.java
@@ -1,19 +1,28 @@
 package com.example.doctruyenapplication;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class AccountFragment extends  Fragment{
+    Button btnLogin,btnLogout;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
         // Nạp bố cục cho fragment này
         View view = inflater.inflate(R.layout.fragment_account, container, false);
 
@@ -28,6 +37,37 @@ public class AccountFragment extends  Fragment{
 
         LinearLayout optionPolicy = view.findViewById(R.id.option_policy);
         optionPolicy.setOnClickListener(v -> navigateToFragment(new AccountFragment()));
+
+        btnLogin = view.findViewById(R.id.btn_login);
+        btnLogout = view.findViewById(R.id.btn_logout);
+        SharedPreferences sharedPreferences = requireActivity().getSharedPreferences("MyAppPrefs", Context.MODE_PRIVATE);
+        boolean isLoggedIn = sharedPreferences.getBoolean("isLoggedIn", false);
+        Log.e("tranhoanggiang",isLoggedIn+"");
+        if (isLoggedIn) {
+            btnLogout.setVisibility(View.VISIBLE);
+            btnLogin.setVisibility(View.GONE);
+        } else {
+            btnLogin.setVisibility(View.VISIBLE);
+            btnLogout.setVisibility(View.GONE);
+        }
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(requireActivity(), Login.class);
+                startActivity(intent);
+            }
+        });
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(requireActivity(), Login.class);
+                startActivity(intent);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putBoolean("isLoggedIn", false); // Lưu cờ đăng nhập
+                editor.apply();
+            }
+        });
+
 
         return view;
     }
--- app/src/main/java/com/example/doctruyenapplication/api/ApiService.java
+++ app/src/main/java/com/example/doctruyenapplication/api/ApiService.java
@@ -1,12 +1,15 @@
 package com.example.doctruyenapplication.api;
 
+import com.example.doctruyenapplication.object.Account;
 import com.example.doctruyenapplication.object.Book;
 import com.example.doctruyenapplication.object.Genre;
 
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 import retrofit2.http.Query;
 
 public interface ApiService {
@@ -16,4 +19,10 @@ public interface ApiService {
     Call<List<Book>> getAllBooks();
     @GET("books")
     Call<List<Book>> getBooks(@Query("pageNumber") int pageNumber, @Query("pageSize") int pageSize);
+
+    @GET("accounts/login")
+    Call<Account>login(@Query("email") String email,@Query("password") String password);
+
+    @POST("accounts/signup")
+    Call<Account>signup(@Body Account account);
 }
